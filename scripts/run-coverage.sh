2b446bf70674cada071fd8246213571abb74d1d9
Alexander Vershilov <alexander.vershilov@gmail.com>
2013-04-07 17:14:45 +0400

    add coverage

---
+++ scripts/run-coverage.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+if [ ! -f 'binary-conduit.cabal' ]; then
+   echo -n "Can't find binary-conduit.cabal; please run this script as"
+   echo -n " ./scripts/run-coverage from within the banary-conduit source"
+   echo " directory"
+   exit 1
+fi
+
+#CABAL_DEV=$(which cabal-dev)
+
+#${CABAL_DEV} configure --enable-tests --flags="coverage"
+#${CABAL_DEV} build || exit 1
+#${CABAL_DEV} test  || exit 1
+
+ghc -fhpc test/Main.hs
+rm Main.tix
+./test/Main
+
+#dist/build/2pc-tests/2pc-tests
+#rm -f system-fileio_tests.tix
+#cabal-dev/bin/system-fileio_tests $@
+
+#EXCLUDES="\
+  #--exclude=Main \
+  #--exclude=FilesystemTests.Posix \
+  #--exclude=FilesystemTests.Util \
+  #--exclude=FilesystemTests.Windows
+#"
+
+hpc markup --srcdir=. Main.tix --destdir=hpc-markup > /dev/null
+hpc report --srcdir=. Main.tix
+
--- test/Main.hs
+++ test/Main.hs
@@ -1,5 +1,7 @@
 import Data.Binary
+import Data.Binary.Put
 import Data.ByteString
+import qualified Data.ByteString.Lazy as LBS
 import Data.Conduit
 import qualified Data.Conduit.List as CL
 import Data.Conduit.Serialization.Binary
@@ -25,9 +27,26 @@ prop_eq xs = monadicIO $ do
         dec :: (Binary a, MonadThrow m) => [a] -> Conduit ByteString m a
         dec _ = conduitDecode
 
+prop_sink :: (Binary a,Eq a) => (a,a) -> Property
+prop_sink (a,b) = monadicIO $ do
+    Right (a',b') <- runExceptionT $ CL.sourceList [a,b]
+                         $= enc a
+                         $$ do a' <- sinkGet get
+                               b' <- CL.consume
+                               return (a',b')
+
+    assert $ a == a'
+    assert $ runPut (put b) == LBS.fromChunks b'
+  where enc :: (Binary a, MonadThrow m) => a -> Conduit a m ByteString
+        enc _ = conduitEncode 
+        dec :: (Binary a, MonadThrow m) => a -> Conduit ByteString m a
+        dec _ = conduitDecode
+
 main = hspec $ describe "conduitEncode =$= conduitDecode == id" $ do
     prop "int" $ (prop_eq :: [Int] -> Property)
     prop "string" $ (prop_eq :: [String] -> Property)
     prop "maybe int" $ (prop_eq :: [Maybe Int] -> Property)
     prop "either int string" $ (prop_eq :: [Either Int String] -> Property)
+    prop "ab" $ (prop_sink :: (Int,Int) -> Property)
+    prop "ab" $ (prop_sink :: (String,String) -> Property)
     
